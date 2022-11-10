47d327043c86e4c0ebefe3f5ed2b31240aee5710
george-wharf <george.wharf@capgemini.com>
2022-11-10 20:33:01 +0000

    EV model, config update

--- EVs/model/model.py
+++ EVs/model/model.py
@@ -7,7 +7,7 @@ from .GridAgent import GridAgent
 import numpy as np
 import pandas as pd
 import itertools
-
+import yaml
 from .datacollection import DataCollector
 
 def compute_gini(model):
@@ -29,29 +29,21 @@ def dead_cars(model):
     return dead_cars
 
 class EVSpaceModel(Model):
-    """ Electric Vehical Model, where cars drive around between locations then pull into charge points
-    """
-
-    def __init__(self, N=100, width=10., height=10., speed=1, N_Charge=10,discharge_rate=0.01,CP_loc='random', 
-                    model_name = 0, MoveType = 'loc', charge_rate = 0.2, CP_capacity=10, grid_spacing = None,
-                    POIs='None'):
-                    
+    """ Electric Vehical Model, where cars drive around between locations then pull into charge points """
+    def __init__(self, **kwargs):  
         """ initalisation of the model, set up agent space, agents and collection modules"""
-        self.num_agents = N
-        self.model_name = model_name
-        self.MoveType = MoveType
-        self.charge_rate = charge_rate
-        tol = 0.01
-        self.width =width
-        self.height = height
-        self.space = ContinuousSpace(width+tol, height+tol,False,x_min=-tol,y_min=-tol) # change to newtwork model with graph as road netowrk
-        self.speed = speed
-        self.completed_trip =0
-        self.N_Charge = N_Charge
+        # Read in configuration files from yaml. If any additional configs then will overwrite base
+        self.read_configs(kwargs)
+        print(self.cfg)
+        # set up model space
+        self.space = ContinuousSpace(self.width+self.tol, self.height+self.tol, False,
+                                        x_min=-self.tol,y_min=-self.tol) 
+        # could change to newtwork model with graph as road netowrk
+        self.completed_trip = 0
 
         # todo set up POI agents which EVs then choose which they want to go to
-        if POIs != 'None':
-            self.POIs = pd.read_csv(POIs).set_index('id')
+        if self.POI_file != 'None':
+            self.POIs = pd.read_csv(self.POI_file).set_index('id')
             self.POIs['uses'] = 0
         else:
             self.POIs = []
@@ -60,17 +52,11 @@ class EVSpaceModel(Model):
         self.schedule = RandomActivation(self)
         self.schedule_list = ['schedule']
         self.datacollector = DataCollector(schedule='schedule',
-            model_reporters={"av_charge": av_charge,
-                            "completed_trip": "completed_trip",
-                            "dead_cars": dead_cars,}, 
-            agent_reporters={"charge": "charge",
-                            'last_location':'last_location',
-                            'next_location':'next_location',
-                            'pos':'pos'} # also could have deadline to work out if can get there one current charge
-                            
-        )
-        for i in range(self.num_agents):
-            a = EVAgent(i, self, speed, discharge_rate)
+            model_reporters = self.cfg['output']['model_reporters'], 
+            agent_reporters = self.cfg['output']['agent_reporters']
+        ) # also could have deadline to work out if can get there one current charge                 
+        for i in range(self.cfg['agent_params']['EVs']['num_agents']):
+            a = EVAgent(i, self)
             self.schedule.add(a)
             # Add the agent to a random space
             self.space.place_agent(a, a.pos)
@@ -83,7 +69,7 @@ class EVSpaceModel(Model):
             agent_reporters={"cars_charging": "cars_charging",
                             'full':'full'}
         )
-        self.gen_CPs(CP_loc, CP_capacity)
+        self.gen_CPs()
         self.datacollector_CP.collect(self)
 
         #set up grid points for analysis
@@ -94,35 +80,48 @@ class EVSpaceModel(Model):
                             "cars_passing": "cars_passing",
                             'X':'X','Y':'Y'}
         )
-        if type(grid_spacing)==int:
-            grid_locs_x = np.linspace(0, self.width, grid_spacing)
-            grid_locs_y = np.linspace(0, self.height, grid_spacing)
-            grid_locs = itertools.product(grid_locs_x, grid_locs_y) 
-            radius = (self.width+self.height) / (2*grid_spacing)
-            for i, pos in enumerate(grid_locs):
-                a = GridAgent(i, self, pos, radius)
-                self.schedule_gridpoints.add(a)
-                # Add the agent to a random space
-                self.space.place_agent(a, a.pos)
+        self.gen_GPs()
         self.schedule_gridpoints.step()
         self.datacollector_gridpoints.collect(self)
-        
+
         self.running = True
 
-    def gen_CPs(self, CP_loc, CP_capacity):
-        # determine charge point locations, either uniform or randomly distribute
-        if isinstance(CP_loc, pd.DataFrame):
-            self.N_Charge = len(CP_loc)
-            names = CP_loc.index
-            x_pos = CP_loc['x'].values
-            y_pos = CP_loc['y'].values
-        elif '.csv' in CP_loc:
-            CP_locs = pd.read_csv(CP_loc).set_index('id')
-            self.N_Charge = len(CP_locs)
-            names = CP_locs.index
-            x_pos = CP_locs['x'].values
-            y_pos = CP_locs['y'].values
-        elif CP_loc == 'uniform':
+
+    def read_configs(self,kwargs):
+        """ Read in configuation parameters from file given to set up model and agents"""
+
+        # firstly read in config file if given, else take base_config.yml 
+        if 'cfg' in kwargs.keys():
+            cfg_file = kwargs['cfg']
+        else:
+            cfg_file = 'configs/base_cfg.yml'
+        
+        with open(cfg_file,'r') as ymlfile:
+            self.cfg = yaml.safe_load(ymlfile)
+        
+        # take model_params from config file and assign them as attributes to model class
+        for k,v in self.cfg['model_params'].items():
+            setattr(self,k,v)
+        
+        # any additional key word arguments given in modle run instance will overwrite any params from the config file.
+        # this is how we can use the vis tool to update params
+        for k,v in kwargs.items():
+            setattr(self,k,v)
+
+    def gen_CPs(self):
+        ''' determine charge point locations, either uniform or randomly distribute '''
+        if isinstance(self.CP_loc, pd.DataFrame):
+            self.N_Charge = len(self.CP_loc)
+            names = self.CP_loc.index
+            x_pos = self.CP_loc['x'].values
+            y_pos = self.CP_loc['y'].values
+        elif '.csv' in self.CP_loc:
+            self.CP_locs = pd.read_csv(self.CP_loc).set_index('id')
+            self.N_Charge = len(self.CP_locs)
+            names = self.CP_locs.index
+            x_pos = self.CP_locs['x'].values
+            y_pos = self.CP_locs['y'].values
+        elif self.CP_loc == 'uniform':
             indices = np.arange(0, self.N_Charge, dtype=float) + 0.5
             r = np.sqrt(indices/self.N_Charge)
             theta = np.pi * (1 + 5**0.5) * indices
@@ -140,12 +139,26 @@ class EVSpaceModel(Model):
             name = str(i)+'_Charge'
 
             pos = charge_locs[i]
-            a = ChargePoint(name, self, pos, CP_capacity)
+            a = ChargePoint(name, self, pos)
             self.schedule_CP.add(a)
 
             # Add the agent to space
             self.space.place_agent(a, pos)
             self.charge_locations[name] = pos
+
+    def gen_GPs(self):
+        ''' determine grid point locations, to collect traffic from EVs passing '''
+        grid_spacing = self.cfg['agent_params']['Grid_Points']['grid_spacing']
+        if type(grid_spacing)==int:
+            grid_locs_x = np.linspace(0, self.width, grid_spacing)
+            grid_locs_y = np.linspace(0, self.height, grid_spacing)
+            grid_locs = itertools.product(grid_locs_x, grid_locs_y) 
+            radius = (self.width+self.height) / (2*grid_spacing)
+            for i, pos in enumerate(grid_locs):
+                a = GridAgent(i, self, pos, radius)
+                self.schedule_gridpoints.add(a)
+                # Add the agent to a random space
+                self.space.place_agent(a, a.pos)
 
     def step(self):
         """ This is the key model function which is run once each step. Here we loop through the agent schedule, which performs each agent step """
@@ -156,7 +169,7 @@ class EVSpaceModel(Model):
         self.collect()
     
     def collect(self):
-        # collect data
+        ''' collect data '''
         self.datacollector.collect(self) # collect agent and model information
         self.datacollector_CP.collect(self) # collect agent and model information
         self.datacollector_gridpoints.collect(self) # collect agent and model information
