373c0a3ba72df7aef8bc69d5f22ad8bbf9b531b3
Fintan-B <fintan.bishop@capgemini.com>
2022-11-16 11:58:08 +0000

    Config File Changes

--- EVs/itterative_locs.py
+++ EVs/itterative_locs.py
@@ -4,11 +4,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 cfgs = ['None', 'configs/Mississauga_cfg.yml', 'configs/Point_Edward_cfg.yml',
-        'configs/east_box.yml',  'configs/west_box.yml']
+        'configs/east_box.yml',  'configs/west_box.yml', 'configs/Bounding_box_1_config.yml']
 
 for cfg in cfgs:
     for seed in range(10):
         print(cfg)
         model = EVSpaceModel(cfg = cfg,seed=seed)  
-        model.run_model(2400) 
+        model.run_model(240) #Change back later to 2400
         model.save()
