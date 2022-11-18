c22261f9a10d754d60e730303f88b7ad3a78a546
samukhia <safal.mukhia@capgemini.com>
2022-11-18 16:07:12 +0000

    update config and itterative_locs

--- EVs/itterative_locs.py
+++ EVs/itterative_locs.py
@@ -1,7 +1,10 @@
 from model.model import *
+import timeit
 
 cfgs = ['None', 'configs/Mississauga_cfg.yml', 'configs/Point_Edward_cfg.yml',
         'configs/east_box.yml', 'configs/west_box.yml']
+
+start = timeit.timeit()
 
 for cfg in cfgs:
     for seed in range(4):
@@ -9,3 +12,6 @@ for cfg in cfgs:
         model = EVSpaceModel(cfg=cfg, ModelP_seed=seed)
         model.run_model(9000)
         model.save()
+
+end = timeit.timeit() - start
+print("Time it took to run: ", end)
